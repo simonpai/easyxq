726d40a1a0349dddff4686aedb97203feabe1bfa
simonpai <simon.pai@gmail.com>
2023-06-23 01:21:08 +0800

    Fine tune aftermath.

--- site/src/component/Aftermath.js
+++ site/src/component/Aftermath.js
@@ -3,14 +3,19 @@ import { Plate, Chalkboard } from './ui/index.js';
 
 export default function Aftermath({ t, mirror, state, onClose, onQuit }) {
   const { result, players } = state;
-  const { type, winner } = result;
+  const { type, winner, reason } = result;
   const human = players[mirror ? 1 : 0];
   return (
     <div className="result" data-type={type} data-winner-color={colors.en(winner)}>
       <Chalkboard>
-        <h1 className="result__phrase">{t(type === 'draw' ? 'draw' : human.color === winner ? 'victory' : 'defeat')}</h1>
-        <hr />
-        <div className="result__btns">
+        <div className="result__header">
+          <h1 className="result__title">{t(type === 'draw' ? 'draw' : human.color === winner ? 'victory' : 'defeat')}</h1>
+          <hr />
+        </div>
+        {
+          reason && <p className="result__subtitle">{t(reason)}</p>
+        }
+        <div className="result__controls">
           <button type="button" className="btn btn-fiberboard-light" onClick={onQuit}>{t('quit')}</button>
         </div>
       </Chalkboard>
--- site/src/component/ui/Chalkboard.js
+++ site/src/component/ui/Chalkboard.js
@@ -2,7 +2,9 @@ export default function Chalkboard({ children }) {
   // TODO: merge class names & attributes
   return (
     <div className="chalkboard">
-      { children }
+      <div className="chalkboard__inner">
+        { children }
+      </div>
     </div>
   );
 }
