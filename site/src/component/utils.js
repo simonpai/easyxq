215ab768ed63f064843d15527d7087d0f5596f2c
simonpai <simon.pai@gmail.com>
2023-08-13 11:44:50 +0800

    Use translate3d

--- site/src/component/utils.js
+++ site/src/component/utils.js
@@ -3,7 +3,7 @@ export function getSidCssProperties(sid) {
   const file = sid & 0x0F;
   const x = 50 + file * 100;
   const y = (9 - rank) * 100;
-  const transform = `translate(${x}%, ${y}%)`;
+  const transform = `translate3d(${x}%, ${y}%, 0)`;
   return { transform };
 }
 
